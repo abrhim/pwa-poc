c52c99967d7a3b0e34eb86bb57328b1a6075a5f6
abram himmer <himmer@adobe.com>
2020-04-16 16:39:39 -0500

    updating links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Src from './src'
 import './App.css';
 
 function App() {
@@ -7,8 +8,8 @@ function App() {
       <p>
         This app can do the following:
          <ul>
-          <li className="check">install to home screen</li>
-          <li className="cross">save data to device</li>
+          <li className="check">install to home screen <Src href="https://create-react-app.dev/docs/making-a-progressive-web-app/" /></li>
+          <li className="cross">save data to device <Src href="https://developers.google.com/web/fundamentals/instant-and-offline/web-storage/offline-for-pwa"/></li>
           <li className="cross">send push notifications</li>
           <li className="cross">push data to some api when internet becomes available</li>
          </ul>
